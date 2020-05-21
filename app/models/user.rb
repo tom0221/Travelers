60248ce59822ea693208915fc040fea9e7d1b858
Tatsuro Muto <tomuto0221@gmail.com>
2020-05-21 16:15:41 +0900

    [finish]いいね機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
   attachment :profile_image
   has_many :post_images, dependent: :destroy
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
 end
