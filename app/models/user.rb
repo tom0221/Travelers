2d45e1495db0a6fae16a80882bf4ef81339b243f
Tatsuro Muto <tomuto0221@gmail.com>
2020-05-22 15:34:07 +0900

    マイページhe投稿一覧

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,24 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy#フォロー機能
   has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy#フォロワー機能
+  has_many :following_user, through: :follower, source: :followed#自分がフォローしている人
+  has_many :follower_user, through: :followed, source: :follower#自分がフォローしている人
 
+  #ユーザーをフォローする
+  def follow(user_id)
+  	follower.create(followed_id: user_id)
+  end
+  #ユーザーのフォローを外す
+  def unfollow(user_id)
+  	follower.find_by(followed_id: user_id).destroy
+  end
+  #フォローしていればtrueを返す
+  def following?(user)
+  	following_user.include?(@user)
+  end
+
+  def post_images
+    return PostImage.where(user_id: self.id)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,16 @@ Rails.application.routes.draw do
   get "/about" => "homes#about"
   get "/users/bye_confirm" => "users#bye_confirm"
   get "/users/thanks" => "users#thanks"
+  #フォロー関連追記
+  post 'follow/:id' => 'relationships#follow', as: 'follow'#フォローする
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow'#フォロー外す
 
   resources :post_images, only: [:new, :create, :index, :show, :edit, :destroy] do
   	resource :favorites, only: [:create, :destroy]
   	resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:create, :show, :edit, :update, :destroy]
+
 
 
 end
