ca81dc860d9340f6c216bf725d692ff242ddedcb
Tatsuro Muto <tomuto0221@gmail.com>
2020-06-09 15:39:04 +0900

    photoレスポンシブまで

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook google_oauth2]
 
   #バリデーション追加
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   validates :name, presence: true
 
   #SNS認証
