7260511aea732c582d007eb1c18b13735c01ff0e
Tatsuro Muto <tomuto0221@gmail.com>
2020-06-13 16:50:11 +0900

    post_images#showへのリンク追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
     @post_images = PostImage.all
+    # @post_image = PostImage.find(params[:id])
   end
 
   def edit
