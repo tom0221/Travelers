99899f2cbe4070f062d70b43c8ba7c9249a26b85
Tatsuro Muto <tomuto0221@gmail.com>
2020-06-14 12:01:10 +0900

    インデント修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,6 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
     @post_images = PostImage.all
-    # @post_image = PostImage.find(params[:id])
   end
 
   def edit
